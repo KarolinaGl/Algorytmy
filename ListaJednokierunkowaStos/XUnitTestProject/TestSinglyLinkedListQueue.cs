dbec64a2f5b50bc680044e244176904c73d278bb
unknown <karolina.gluszek6@gmail.com>
2020-02-11 23:03:58 +0100

    Dodanie kolejki

---
+++ ListaJednokierunkowaStos/XUnitTestProject/TestSinglyLinkedListQueue.cs
@@ -0,0 +1,31 @@
+﻿using SinglyLinkedListStack;
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class TestSinglyLinkedListQueue
+    {
+        [Fact]
+        public void TestIsEmpty()
+        {
+            IQueue<int> sut = new SinglyLinkedListQueue<int>();
+            Assert.True(sut.IsEmpty());
+
+            sut.Push(5);
+            Assert.False(sut.IsEmpty());
+        }
+
+        [Fact]
+        public void TestPush()
+        {
+            IQueue<int> sut = new SinglyLinkedListQueue<int>();
+            Assert.Throws<IndexOutOfRangeException>(() => sut.Front());
+            sut.Push(3);
+            Assert.Equal(3, sut.Front());
+            sut.Push(4);
+            sut.Push(5);
+            Assert.Equal(3, sut.Front());
+        }
+    }
+}
